e6502140fb002252f54df1e4f8df076f58be74e1
Jérôme Mahuet <jerome.mahuet@gmail.com>
2023-09-22 10:47:03 +0200

    tracing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::http::StatusCode;
 use axum::{routing::get, Router};
-use tower_http::trace::TraceLayer;
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
@@ -8,16 +9,23 @@ use tracing_subscriber::util::SubscriberInitExt;
 async fn main() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "jeromem_dev=debug".into()),
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(Level::INFO));
+
     let app = Router::new()
         .route("/health", get(StatusCode::OK))
-        .layer(TraceLayer::new_for_http());
+        .layer(trace_layer);
 
-    axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
+    let addr = "127.0.0.1:8080";
+    tracing::info!("listening on {}", addr);
+
+    axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
