456c725e76cecc06605c5b374a21e8089a99fee7
Jérôme Mahuet <jerome.mahuet@gmail.com>
2023-09-22 20:07:26 +0200

    refactor middleware

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use axum::body::StreamBody;
 use axum::response::IntoResponse;
-use axum::ServiceExt;
 use axum::{routing::get, Router};
 use hyper::header;
 use jeromem_dev::routes::health_check_handler;
 use jeromem_dev::routes::not_found_handler;
 use jeromem_dev::telemetry::setup_tracing;
 use tokio_util::io::ReaderStream;
-use tower::Layer;
+use tower::ServiceBuilder;
+use tower_http::compression::CompressionLayer;
 use tower_http::normalize_path::NormalizePathLayer;
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new()
+    let app = Router::new()
         .nest_service("/static", ServeDir::new("static"))
         .route(
             "/robots.txt",
@@ -24,17 +24,19 @@ async fn main() {
             get(|| serve_from_static_folder("favicon.ico", "image/x-icon")),
         )
         .route("/health", get(health_check_handler))
-        .layer(NormalizePathLayer::trim_trailing_slash())
         .layer(setup_tracing())
+        .layer(
+            ServiceBuilder::new()
+                .layer(NormalizePathLayer::trim_trailing_slash())
+                .layer(CompressionLayer::new()),
+        )
         .fallback(not_found_handler);
-
-    let router = NormalizePathLayer::trim_trailing_slash().layer(router);
 
     let addr = "127.0.0.1:8080";
     tracing::info!("listening on {}", addr);
 
     axum::Server::bind(&addr.parse().unwrap())
-        .serve(router.into_make_service())
+        .serve(app.into_make_service())
         .await
         .unwrap();
 }
